5ce5b223a04d4fa83365e48739649aeec6f79469
vityaman <vityaman.dev@yandex.ru>
2025-11-11 11:57:20 +0300

    Add VSCode Extension + LSP stub

---
+++ client/src/client.ts
@@ -0,0 +1,52 @@
+import * as path from 'path';
+import { ExtensionContext } from 'vscode';
+
+import {
+    LanguageClient,
+    LanguageClientOptions,
+    ServerOptions,
+    TransportKind
+} from 'vscode-languageclient/node';
+
+let client: LanguageClient | undefined = undefined;
+
+export function activate(context: ExtensionContext) {
+    let module = context.asAbsolutePath(path.join('server', 'out', 'server.js'));
+
+    const serverOptions: ServerOptions = {
+        run: {
+            module: module,
+            transport: TransportKind.ipc,
+        },
+        debug: {
+            module: module,
+            transport: TransportKind.ipc,
+        }
+    };
+
+    const clientOptions: LanguageClientOptions = {
+        documentSelector: [
+            {
+                scheme: 'file',
+                language: 'plaintext',
+            },
+        ],
+    };
+
+    client = new LanguageClient(
+        'languageServerYQLs',
+        'YQLs LSP Server',
+        serverOptions,
+        clientOptions,
+    );
+
+    client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+    if (!client) {
+        return undefined;
+    }
+
+    return client.stop();
+}
