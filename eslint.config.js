005aa7c9f4fe59f0fd477ffb2769a3056ed36abf
Ilya Babanov <an1mepacan@yandex.ru>
2025-11-12 13:33:36 +0300

    linter fixed CI/CD 2

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import stylistic from '@stylistic/eslint-plugin'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['out', '**/out/**']),
+  globalIgnores(['out', '**/out/**', 'eslint.config.js']),
   {
     files: ['{client,server}/**/src/*.ts'],
     extends: [
